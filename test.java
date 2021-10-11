7af22bab4d79edfed2fb45e228b64cc71505968c
Hoony <rladudgns456@naver.com>
2021-10-11 19:15:56 +0900

    update 1일차.md

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    
+
+    
+    public static void main(String[] args) {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(path);
+        dispatcher.forward(request, response);
+    }
+}
